db44d1b66d2012b929a3114b0b8059cc12e61dff
OrnelaDehayem <o.dehayem@gmail.com>
2024-07-14 01:22:44 +0200

    Update function_to_drop_species.R

--- function_to_drop_species.R
+++ function_to_drop_species.R
@@ -1,70 +1,104 @@
-#packages needed
+# Load necessary library
 library(ape)
 
 
 
-drop_youngest_species <- function(trees, branching_times)
-{
+# Function to drop the youngest species from a tree
+drop_youngest_species <- function(trees, branching_times) {
   
+  # Number of species
   S <- length(branching_times) + 1
-  tip_lengths <- setNames(trees$edge.length[sapply(1:S,
-                                                   function(x,y) which(y == x),y = trees$edge[,2])],trees$tip.label)
   
+  # Get the tip lengths for each species
+  tip_lengths <- setNames(
+    trees$edge.length[sapply(1:S, function(x, y) which(y == x), y = trees$edge[, 2])],
+    trees$tip.label
+  )
+  
+  # Identify the species with the minimum tip length
   branch_to_drop_min <- names(which(tip_lengths == min(tip_lengths)))
-  # Check if branch_to_drop_min has multiple values
+  
+  # If there are multiple youngest species, select the first one
   if (length(branch_to_drop_min) > 1) {
-    #Drop only one of the youngest species
     branch_to_drop_min <- branch_to_drop_min[1]
   }
-  tree_reduced <- ape::drop.tip(trees, branch_to_drop_min)
-  brts <- branching.times(tree_reduced)
-  sorted_brts <- sort(as.vector(brts), decreasing = TRUE)
-  return(list(tip_lengths = tip_lengths, tree_reduced = tree_reduced, sorted_brts = sorted_brts))
   
+  # Drop the identified species from the tree
+  tree_reduced <- ape::drop.tip(trees, branch_to_drop_min)
+  
+  # Get the branching times of the reduced tree
+  brts <- branching.times(tree_reduced)
+  
+  # Sort branching times in decreasing order
+  sorted_brts <- sort(as.vector(brts), decreasing = TRUE)
+  
+  # Return the result as a list
+  return(list(tip_lengths = tip_lengths, tree_reduced = tree_reduced, sorted_brts = sorted_brts))
 }
 
 ###############
-drop_oldest_species <- function(trees, branching_times)
-{
+# Function to drop the oldest species from a tree
+drop_oldest_species <- function(trees, branching_times) {
   
+  # Number of species
   S <- length(branching_times) + 1
-  tip_lengths <- setNames(trees$edge.length[sapply(1:S,
-                                                   function(x,y) which(y == x),y = trees$edge[,2])],trees$tip.label)
   
+  # Get the tip lengths for each species
+  tip_lengths <- setNames(
+    trees$edge.length[sapply(1:S, function(x, y) which(y == x), y = trees$edge[, 2])],
+    trees$tip.label
+  )
+  
+  # Identify the species with the maximum tip length
   branch_to_drop_max <- names(which(tip_lengths == max(tip_lengths)))
-  # Check if branch_to_drop_min has multiple values
+  
+  # If there are multiple oldest species, select the first one
   if (length(branch_to_drop_max) > 1) {
-    # Drop only one of the youngest species
     branch_to_drop_max <- branch_to_drop_max[1]
   }
+  
+  # Drop the identified species from the tree
   tree_reduced <- ape::drop.tip(trees, branch_to_drop_max)
+  
+  # Get the branching times of the reduced tree
   brts <- branching.times(tree_reduced)
+  
+  # Sort branching times in decreasing order
   sorted_brts <- sort(as.vector(brts), decreasing = TRUE)
+  
+  # Return the result as a list
   return(list(tip_lengths = tip_lengths, tree_reduced = tree_reduced, sorted_brts = sorted_brts))
-  
 }
-
 ###############
-drop_random_species <- function(trees, branching_times)
-{
+# Function to drop a random species from a tree
+drop_random_species <- function(trees, branching_times) {
   
+  # Number of species
   S <- length(branching_times) + 1
-  tip_lengths <- setNames(trees$edge.length[sapply(1:S,
-                                                   function(x,y) which(y == x),y = trees$edge[,2])],trees$tip.label)
   
+  # Get the tip lengths for each species
+  tip_lengths <- setNames(
+    trees$edge.length[sapply(1:S, function(x, y) which(y == x), y = trees$edge[, 2])],
+    trees$tip.label
+  )
+  
+  # Randomly sample one tip length
   sampled_length <- sample(tip_lengths, 1)
   
-  # Get the indices of elements equal to the sampled length
+  # Get the indices of species with the sampled tip length
   matching_indices <- which(tip_lengths == sampled_length)
   
-  # drop the species
-  
-  
+  # Drop the identified species from the tree
   tree_reduced <- ape::drop.tip(trees, names(tip_lengths[matching_indices]))
-  brts <- branching.times(tree_reduced)
-  sorted_brts <- sort(as.vector(brts), decreasing = TRUE)
-  return(list(tip_lengths = tip_lengths, tree_reduced = tree_reduced, sorted_brts = sorted_brts))
   
+  # Get the branching times of the reduced tree
+  brts <- branching.times(tree_reduced)
+  
+  # Sort branching times in decreasing order
+  sorted_brts <- sort(as.vector(brts), decreasing = TRUE)
+  
+  # Return the result as a list
+  return(list(tip_lengths = tip_lengths, tree_reduced = tree_reduced, sorted_brts = sorted_brts))
 }
 
 
