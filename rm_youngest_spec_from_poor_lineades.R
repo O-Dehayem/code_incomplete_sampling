0dbba2db54df728c24d8d12360189b858d5f787b
OrnelaDehayem <o.dehayem@gmail.com>
2024-07-19 10:22:16 +0200

    Update rm_youngest_spec_from_poor_lineages.R

--- rm_youngest_spec_from_poor_lineades.R
+++ rm_youngest_spec_from_poor_lineades.R
@@ -1,1 +1,8 @@
+#' This function modifies a dataset of simulated islands by removing the youngest species from poor lineages.
+
+ 
+#' Load necessary packages
+library(ape)
+library(DAISIE)
+library(DDD)
 
