969b85c337f9d055961e1d9db0746a67a1133a6d
OrnelaDehayem <o.dehayem@gmail.com>
2024-07-19 10:26:35 +0200

    Update and rename rm_rand_spec_from_rich_lineades.R to rm_rand_spec_from_rich_lineages.R

---
+++ rm_rand_spec_from_rich_lineages.R
@@ -0,0 +1,8 @@
+#' This function modifies a dataset of simulated islands by removing random species from rich lineages.
+
+ 
+#' Load necessary packages
+library(ape)
+library(DAISIE)
+library(DDD)
+
