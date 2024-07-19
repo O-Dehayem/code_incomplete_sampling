27b40d2ba5de5066887220caba0ec9e4dbd0b350
OrnelaDehayem <o.dehayem@gmail.com>
2024-07-19 10:21:49 +0200

    Update and rename rm_youngest_spec_from_rand_lineades.R to rm_youngest_spec_from_rand_lineages.R

---
+++ rm_youngest_spec_from_rand_lineages.R
@@ -0,0 +1,8 @@
+#' This function modifies a dataset of simulated islands by removing the youngest species from random lineages.
+
+ 
+#' Load necessary packages
+library(ape)
+library(DAISIE)
+library(DDD)
+
