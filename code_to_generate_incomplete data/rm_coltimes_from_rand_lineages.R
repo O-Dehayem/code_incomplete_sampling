6bf102798207dc1c8942b666974824a1aaa55c19
Ornela Dehayem Nanwou <o.dehayem@gmail.com>
2025-03-21 23:37:38 +0100

    Update rm_coltimes_from_rand_lineages.R

--- code_to_generate_incomplete data/rm_coltimes_from_rand_lineages.R
+++ code_to_generate_incomplete data/rm_coltimes_from_rand_lineages.R
@@ -9,12 +9,6 @@
 #' @return A modified list of simulated datasets with adjusted colonization times.
 #'
 
-#' Load necessary library
-library(ape)
-library(DAISIE)
-library(DDD)
-
-
 
 remove_coltimes_from_random <- function(rich_lineage_threshold, percentage_coltimes_to_remove, data_sims_G) {
 
