b35b8f5dc243b17c0a6462e4fcc665ba1b0690eb
Ornela Dehayem Nanwou <o.dehayem@gmail.com>
2025-03-21 23:37:54 +0100

    Update rm_coltimes_from_rich_lineages.R

--- code_to_generate_incomplete data/rm_coltimes_from_rich_lineages.R
+++ code_to_generate_incomplete data/rm_coltimes_from_rich_lineages.R
@@ -8,10 +8,7 @@
 #' @param data_sims_G A list of simulated datasets.
 #' @return A modified list of simulated datasets with adjusted colonization times.
 #'
-#' Load necessary library
-library(ape)
-library(DAISIE)
-library(DDD)
+
 
 
 remove_coltimes_from_rich <- function(rich_lineage_threshold, percentage_coltimes_to_remove, data_sims_G) {
